4e7060b8f1edb3cc484cf5cb668055574da99785
kouuuki <mark.web.888@gmail.com>
2018-05-27 18:56:29 +0900

    add java files

---
+++ Calc.java
@@ -0,0 +1,20 @@
+public class Calc {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 30;
+    int total = a * b;
+
+    System.out.println(total);
+  }
+
+  public static void tasu(int x) {
+    System.out.println(x * x);
+  }
+
+  public static void main() {
+    System.out.println("hello world");
+  }
+}
+
+
+hello
---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+  public static void main(String[] srgs) {
+    int a = 1100;
+    double b = 3.43443;
+    int[] array;
+    array = new int[2];
+
+    array[0] = 100;
+    array[2] = 200;
+
+    if (a > 11) {
+      System.out.println(a);
+    } else {
+      System.out.println(b);
+    }
+  }
+}
